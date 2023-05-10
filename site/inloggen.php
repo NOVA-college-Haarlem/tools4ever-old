511cd96470daaf3180b3d0fbb73357b3adcb8afa
David van Oudheusden <doudheusden@novacollege.nl>
2023-05-10 13:52:48 +0200

    inloggen gebouwd

---
+++ site/inloggen.php
@@ -0,0 +1,33 @@
+<?php include 'header.php' ;
+
+?>
+
+<main class="form-signin w-100 m-auto">
+    <form method="post" action="verwerk-inloggen.php">
+
+        <h1 class="h3 mb-3 fw-normal">Log in aub</h1>
+
+        <div class="form-floating">
+            <label for="floatingInput">Email address</label>
+            <input type="email" class="form-control" id="floatingInput" name="email" placeholder="name@example.com">
+        </div>
+        <div class="form-floating">
+            <label for="floatingPassword">Password</label>
+            <input type="password" class="form-control" id="floatingPassword" name="password" placeholder="Password">
+        </div>
+
+        <div class="checkbox mb-3">
+            <label>
+                <input type="checkbox" value="remember-me"> Remember me
+            </label>
+        </div>
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2023</p>
+    </form>
+</main>
+
+
+
+</body>
+
+</html>
---
+++ site/verwerk-inloggen.php
@@ -0,0 +1,60 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header($_SERVER["SERVER_PROTOCOL"] . " 405 Method Not Allowed", true, 405);
+    include '405.php';
+    exit;
+}
+
+$email  = $_POST['email'];
+$password  = $_POST['password'];
+
+require 'database.php';
+
+$sql = "SELECT * FROM users WHERE email = '$email' ";
+
+$result = mysqli_query($conn, $sql);
+
+$user = mysqli_fetch_assoc($result);
+
+
+if (!is_array($user)) {
+    header("location: inloggen.php");
+    exit;
+}
+
+if ($user['password'] === $_POST['password']) {
+
+    session_start();
+
+    $_SESSION['isIngelogd'] = true;
+    
+    $_SESSION['voornaam']  = $user['firstname'];
+    
+    $_SESSION['role'] = $user['role'];
+
+
+    switch ($user['role']) {
+        case 'admin':
+            header("location: admin-dashboard.php");
+            exit;
+            break;
+        case 'employee':
+            header("location: dashboard.php");
+            exit;
+            break;
+        case 'customer':
+            header("location: store.php");
+            exit;
+            break;
+
+        default:
+            header("location: store.php");
+            exit;
+            break;
+    }
+}
+
+
+header("location: inloggen.php");
+exit;
