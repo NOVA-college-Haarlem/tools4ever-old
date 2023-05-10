89c431618e2336dfdb830a6a9e1b109231631ff5
David van Oudheusden <doudheusden@novacollege.nl>
2023-05-10 13:52:32 +0200

    producten pagina gemaakt

---
+++ site/index.php
@@ -0,0 +1,51 @@
+<?php include 'header.php' ?>
+
+<?php
+
+require 'database.php';
+
+
+$sql = "SELECT * FROM tools";
+
+$result = mysqli_query($conn, $sql);
+
+$tools = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+
+?>
+<header>
+    <h1>
+        Welkom bij Tools4ever
+    </h1>
+    <p>
+        Op deze site vind je alle informatie over de tools die te koop zijn bij Tools4ever
+    </p>
+</header>
+<main>
+    <section class="search">
+        <form action="vewerk-zoek.php" method="post">
+            <label for="">zoek</label>
+            <input type="text" name="zoekveld" id="">
+            <button type="submit">Zoek!</button>
+        </form>
+    </section>
+    <div class="product-container">
+
+        <div class="products-grid">
+            <?php foreach ($tools as $tool) : ?>
+                <div class="product">
+                    <h3 class="product-name"><?php echo $tool['tool_name']; ?></h3>
+                    <!-- <div class="product-category"><?php echo $tool['tool_category']; ?></div> -->
+                    <div class="product-price">&euro;<?php echo $tool['tool_price']; ?></div>
+
+                    <img src="images/<?php echo $tool['tool_image']; ?>" alt="" class="product-image">
+                    <a href="tool-detail.php?id=<?php echo $tool['tool_id']; ?>">bekijk</a>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</main>
+</body>
+
+</html>
